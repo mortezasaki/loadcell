39bd2606fe5885e18d55fc89c12ace98875931f3
mortezasaki <mortezasaki91@gmail.com>
2022-09-26 11:12:01 +0330

    refator: remove code comments

--- app/app.ino
+++ app/app.ino
@@ -25,54 +25,18 @@ void setup() {
   }
   lcd.init();
   lcd.backlight();
-  lcd.print("Hello, world!");
-  pinMode(add_btn_pin, INPUT);
-  Serial.begin(57600);
-  Serial.println("HX711 Demo by Satyam Singh");
-  Serial.println("Initializing the scale");
+  lcd.print("Starting...");
  
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
  
-  // Serial.println("Before setting up the scale:");
-  // Serial.print("read: \t\t");
-  // Serial.println(scale.read());      // print a raw reading from the ADC
- 
-  // Serial.print("read average: \t\t");
-  // Serial.println(scale.read_average(20));   // print the average of 20 readings from the ADC
- 
-  // Serial.print("get value: \t\t");
-  // Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight (not set yet)
- 
-  // Serial.print("get units: \t\t");
-  // Serial.println(scale.get_units(5), 1);  // print the average of 5 readings from the ADC minus tare weight (not set) divided
-  //           // by the SCALE parameter (not set yet)
-            
-  // scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
-  // //scale.set_scale(-471.497);                      // this value is obtained by calibrating the scale with known weights; see the README for details
   scale.tare();               // reset the scale to 0
   scale.set_scale(31.457);
- 
-  // Serial.println("After setting up the scale:");
- 
-  // Serial.print("read: \t\t");
-  // Serial.println(scale.read());                 // print a raw reading from the ADC
- 
-  // Serial.print("read average: \t\t");
-  // Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
- 
-  // Serial.print("get value: \t\t");
-  // Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight, set with tare()
- 
-  // Serial.print("get units: \t\t");
-  // Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
-  //           // by the SCALE parameter set with set_scale
- 
-  // Serial.println("Readings:");
 }
  
 void loop() {
   double kilograms;
   kilograms = scale.get_units(20)/1000;
+  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(kilograms, 2);
   lcd.println(" kg");
